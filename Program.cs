4fe5a3abc154cf1c154775b12fb81e8e8a70d16c
sgca85 <162612350+sgca85@users.noreply.github.com>
2025-05-05 09:45:04 -0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,35 @@
+﻿Console.WriteLine("--- Fluxograma Da Engenharia ---\n");
+
+Console.Write("O objeto está se movendo (S/N)? ");
+bool estaSeMovendo = Console.ReadLine()!.Trim().Substring(0, 1).ToUpper() == "S";
+
+Console.Write("Deveria se mover (S/N)? ");
+bool deveria = Console.ReadLine()!.Trim().Substring(0, 1).ToUpper() == "S";
+
+string mensagem;
+
+if (!estaSeMovendo)
+{
+    if (!deveria)
+    {
+        mensagem = "Excelente!";
+    }
+    else
+    {
+        mensagem = "Aplique WD-40.";
+    }
+}
+else
+{
+    if (deveria)
+    {
+        mensagem = "Excelente!";
+    }
+    else
+    {
+        mensagem = "Aplique Silver Tape.";
+    }
+}
+
+Console.WriteLine();
+Console.WriteLine(mensagem);
